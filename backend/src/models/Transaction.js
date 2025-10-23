bde608ab39f914f7f924140a4c2a179a66af37ba
Ario Shakeri <itsario900@gmail.com>
2025-10-23 10:12:07 +0330

    Added collectionrequest

--- backend/src/models/Transaction.js
+++ backend/src/models/Transaction.js
@@ -2,7 +2,7 @@ import { Schema, model } from 'mongoose';
 
 const transactionSchema = Schema(
     {
-        book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
+        request: { type: Schema.Types.ObjectId, ref: 'CollectionRequest', required: true },
         user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
         type: { type: String, enum: ['borrow', 'purchase'], required: true },
         startDate: { type: Date, default: Date.now },
