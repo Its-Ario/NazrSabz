7addcd2cb0183f9f818a5f9b38a5a276e1d5834f
Ario Shakeri <itsario900@gmail.com>
2025-11-11 20:24:12 +0330

    Removed tailwind from frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,9 @@
 /** @type {import('vite').UserConfig} */
-import tailwindcss from '@tailwindcss/vite';
 
 export default {
     server: {
         proxy: {
             '/api': 'http://localhost:3000',
         },
-        plugins: [tailwindcss()],
     },
 };
