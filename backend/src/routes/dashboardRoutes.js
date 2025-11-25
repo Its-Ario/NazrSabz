d7b048b830eb3637b286214352d3a0f87f65bbb9
Ario Shakeri <itsario900@gmail.com>
2025-11-25 16:00:50 +0330

    Fixed dashboard middleware

--- backend/src/routes/dashboardRoutes.js
+++ backend/src/routes/dashboardRoutes.js
@@ -3,7 +3,7 @@ const router = Router();
 import auth, { isAdmin } from '../middleware/authMiddleware.js';
 import { getDashboardData, getAdminDashboardData } from '../controllers/dashboardController.js';
 
-router.use(auth, isAdmin);
+router.use(auth);
 
 router.get('/', getDashboardData);
 router.get('/admin', isAdmin, getAdminDashboardData);
